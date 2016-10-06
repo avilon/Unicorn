d6ded203a23daa294bbe7c7ac6043856bd625e3a
bykov.ad <bykov.ad@gmail.com>
2016-10-06 11:56:43 +0500

    Add test for Engine

---
+++ UnitTests/TestEngine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Unicorn;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestEngine
+    {
+        [TestMethod]
+        public void TestCreate()
+        {
+            Engine engine = new Engine();
+            Assert.IsNotNull(engine);
+        }
+    }
+}
